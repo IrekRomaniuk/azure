6e74fb01c3b119745133bc8029545fc5824aaa2d
irom77 <aniairom0>
2018-10-15 13:34:53 -0400

    testing

--- vm-simple-linux/script1.py
+++ vm-simple-linux/script1.py
@@ -1,6 +1,5 @@
 import subprocess
 print("This line will be printed.")
-#subprocess.call("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -", shell=True)
 
 #subprocess.call("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -", shell=True) 
 try:

bbb763677e1dfed908a2423a8ae6feca5f74a71d
irom77 <aniairom0>
2018-10-15 13:40:44 -0400

    testing

--- vm-simple-linux/script1.py
+++ vm-simple-linux/script1.py
@@ -1,9 +1,11 @@
 import subprocess
+#import shlex
 print("This line will be printed.")
 
-#subprocess.call("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -", shell=True) 
+subprocess.call("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -", shell=True) 
+"""
 try:
     subprocess.check_output(shlex.split("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -"))
 except subprocess.CalledProcessError, e:
     looger.info("[ERROR]: cron update error")
-    return 'false'
+"""    

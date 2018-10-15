7603a68461942e5fb085903eb3d08f697eb714f3
irom77 <aniairom0>
2018-10-15 13:34:33 -0400

    testing updates

--- vm-simple-linux/script1.py
+++ vm-simple-linux/script1.py
@@ -1,3 +1,10 @@
 import subprocess
 print("This line will be printed.")
-subprocess.call("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -", shell=True)
+#subprocess.call("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -", shell=True)
+
+#subprocess.call("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -", shell=True) 
+try:
+    subprocess.check_output(shlex.split("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -"))
+except subprocess.CalledProcessError, e:
+    looger.info("[ERROR]: cron update error")
+    return 'false'

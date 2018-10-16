d3909d3eb4a6176c2986463e20f120451cf56957
irom77 <aniairom0>
2018-10-16 15:16:52 -0400

    using fixed IP in two tire sample

--- vm-simple-linux/script1.py
+++ vm-simple-linux/script1.py
@@ -2,7 +2,7 @@ import subprocess
 #import shlex
 print("This line will be printed.")
 
-subprocess.call("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -", shell=True) 
+subprocess.call("echo \"* * * * * /bin/ping 8.8.8.8 -c 3\" | crontab -", shell=True) 
 """
 try:
     subprocess.check_output(shlex.split("echo \"* * * * * /bin/ping 10.34.1.1 -c 3\" | crontab -"))
